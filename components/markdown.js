84cf61b6eb2c970034c4d78d9b4254aca4f08951
MystPi <86574651+MystPi@users.noreply.github.com>
2022-05-23 13:10:53 -0400

    feat: allow iframes from Scratch

--- components/markdown.js
+++ components/markdown.js
@@ -2,6 +2,15 @@ import { TypographyStylesProvider } from '@mantine/core';
 import { marked } from 'marked';
 import DOMPurify from 'isomorphic-dompurify';
 
+DOMPurify.addHook('uponSanitizeElement', (node, data) => {
+  if (data.tagName === 'iframe') {
+    const src = node.getAttribute('src') || '';
+    if (!src.startsWith('https://scratch.mit.edu/')) {
+      return node.parentNode?.removeChild(node);
+    }
+  }
+});
+
 function removeNestedCodeBlocks(html) {
   return html
     .replace(/<pre><code(?: class="(.*?)")?>/g, '<pre class="$1">')
@@ -14,7 +23,11 @@ export default function Markdown({ value }) {
       <div
         dangerouslySetInnerHTML={{
           __html: DOMPurify.sanitize(
-            removeNestedCodeBlocks(marked.parse(value))
+            removeNestedCodeBlocks(marked.parse(value)),
+            {
+              ADD_TAGS: ['iframe'],
+              ADD_ATTR: ['allowtransparency', 'frameborder', 'scrolling'],
+            }
           ),
         }}
       />
