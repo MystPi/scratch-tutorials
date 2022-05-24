4bbb819398f7a42ec914f08a03746a328892a445
MystPi <86574651+MystPi@users.noreply.github.com>
2022-05-24 17:29:00 -0400

    feat: equal-height cards

--- components/tutorialCard.js
+++ components/tutorialCard.js
@@ -1,32 +1,34 @@
-import { Card, Button, Text, Anchor } from '@mantine/core';
+import { Card, Group, Button, Text, Anchor } from '@mantine/core';
 import Link from 'next/link';
 
 export default function TutorialCard({ tutorial }) {
   return (
-    <Card shadow="sm" p="lg">
-      <Text size="lg" lineClamp={2}>
-        {tutorial.title}
-      </Text>
-      <Text color="dimmed" size="xs">
-        By{' '}
-        <Link href={`/tutorials/user/${tutorial.by}`} passHref>
-          <Anchor size="xs">{tutorial.by}</Anchor>
+    <Card shadow="sm" p="lg" sx={{ height: '100%' }}>
+      <Group direction="column" spacing={0} sx={{ height: '100%' }}>
+        <Text size="lg" lineClamp={2}>
+          {tutorial.title}
+        </Text>
+        <Text color="dimmed" size="xs">
+          By{' '}
+          <Link href={`/tutorials/user/${tutorial.by}`} passHref>
+            <Anchor size="xs">{tutorial.by}</Anchor>
+          </Link>
+        </Text>
+        <Text
+          my="sm"
+          size="sm"
+          color="gray"
+          lineClamp={4}
+          style={{ wordBreak: 'normal', overflowWrap: 'anywhere' }}
+        >
+          {tutorial.contents.slice(0, 800)}
+        </Text>
+        <Link href={`/tutorials/id/${tutorial.id}`} passHref>
+          <Button mt="auto" component="a" variant="light" fullWidth>
+            View
+          </Button>
         </Link>
-      </Text>
-      <Text
-        my="sm"
-        size="sm"
-        color="gray"
-        lineClamp={4}
-        style={{ wordBreak: 'normal', overflowWrap: 'anywhere' }}
-      >
-        {tutorial.contents.slice(0, 800)}
-      </Text>
-      <Link href={`/tutorials/id/${tutorial.id}`} passHref>
-        <Button component="a" variant="light" fullWidth>
-          View
-        </Button>
-      </Link>
+      </Group>
     </Card>
   );
 }
