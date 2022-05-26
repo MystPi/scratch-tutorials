10a2540dc63f6c188c3c9d58c772108c1396901c
MystPi <86574651+MystPi@users.noreply.github.com>
2022-05-26 17:34:32 -0400

    feat: tutorial pagination

--- components/tutorialGroup.js
+++ components/tutorialGroup.js
@@ -1,22 +1,34 @@
-import { Grid } from '@mantine/core';
+import { Grid, Pagination } from '@mantine/core';
+import { useState } from 'react';
 import TutorialCard from './tutorialCard';
 
 export default function TutorialGroup({ tutorials, sort }) {
+  const [page, setPage] = useState(1);
+
   return (
-    <Grid>
-      {tutorials
-        .sort((a, b) => {
-          if (sort === 'new') {
-            return b.id - a.id;
-          } else if (sort === 'old') {
-            return a.id - b.id;
-          }
-        })
-        .map((tutorial) => (
-          <Grid.Col md={4} sm={6} key={tutorial.id}>
-            <TutorialCard tutorial={tutorial} />
-          </Grid.Col>
-        ))}
-    </Grid>
+    <>
+      <Pagination
+        mb="xl"
+        page={page}
+        onChange={setPage}
+        total={Math.ceil(tutorials.length / 6)}
+      />
+      <Grid>
+        {tutorials
+          .sort((a, b) => {
+            if (sort === 'new') {
+              return b.id - a.id;
+            } else if (sort === 'old') {
+              return a.id - b.id;
+            }
+          })
+          .slice((page - 1) * 6, page * 6)
+          .map((tutorial) => (
+            <Grid.Col md={4} sm={6} key={tutorial.id}>
+              <TutorialCard tutorial={tutorial} />
+            </Grid.Col>
+          ))}
+      </Grid>
+    </>
   );
 }
--- pages/api/tutorials/all.js
+++ pages/api/tutorials/all.js
@@ -1,14 +1,10 @@
-import { getAllTutorials, paginateTutorials } from 'lib/db';
+import { getAllTutorials } from 'lib/db';
 
 export default async function handle(req, res) {
   if (req.method === 'GET') {
     let tutorials;
 
-    if (req.query.page) {
-      tutorials = await paginateTutorials(req.query.page);
-    } else {
-      tutorials = await getAllTutorials();
-    }
+    tutorials = await getAllTutorials();
 
     res.json(tutorials);
   } else {
