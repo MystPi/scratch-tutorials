a8dae84aa1615715564e7f8babbeab6911d0a89a
MystPi <86574651+MystPi@users.noreply.github.com>
2022-07-07 08:39:47 -0400

    chore: update mods list

--- pages/mods.js
+++ pages/mods.js
@@ -2,7 +2,7 @@ import { Title, Text, TypographyStylesProvider } from '@mantine/core';
 import Layout from 'components/layout';
 
 export default function Moderators() {
-  const mods = ['NFlex23', 'Chiroyce', 'Lockstart', 'kccuber', 'Adzboy'];
+  const mods = ['NFlex23', 'Chiroyce', 'Lockstart', 'kccuber', 'Adzboy', 'MaterArc'];
 
   return (
     <Layout title="Moderators">
